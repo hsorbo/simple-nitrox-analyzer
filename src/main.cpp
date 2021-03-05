36411d61c7d7d4adc3be230ce044c3723c276c62
Håvard Sørbø <havard@hsorbo.no>
2021-03-06 00:16:06 +0100

    Dep-fixes to make it compile

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <Arduino.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_ADS1015.h>
+#include <EEPROM.h>
+#include <SPI.h>
+#include <Wire.h>
 
 void setup() {
   // put your setup code here, to run once:
